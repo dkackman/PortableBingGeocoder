79cdc5f21c4528f25a325662c277722710960a39
Don Kackman <dkackman@gmail.com>
2013-11-29 21:13:03 -0600

    code comments

--- BingGeoCoder/UserContext.cs
+++ BingGeoCoder/UserContext.cs
@@ -2,23 +2,44 @@
 
 namespace BingGeocoder
 {
+    /// <summary>
+    /// Used to provide more accurate results based on the user locations
+    /// </summary>
     public class UserContext
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ip">The user's ip address</param>
         public UserContext(string ip)
         {
             IPAddress = ip;
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="location">The user's location</param>
         public UserContext(Tuple<double, double> location)
         {
             Location = location;
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mapView">bounding box of a map view</param>
         public UserContext(Tuple<double, double, double, double> mapView)
         {
             MapView = mapView;
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ip">The user's ip address</param>
+        /// <param name="location">The user's location</param>
+        /// <param name="mapView">bounding box of a map view</param>
         public UserContext(string ip, Tuple<double, double> location, Tuple<double, double, double, double> mapView)
         {
             IPAddress = ip;
@@ -26,10 +47,19 @@ namespace BingGeocoder
             MapView = mapView;
         }
 
+        /// <summary>
+        /// ip address of the user
+        /// </summary>
         public string IPAddress { get; private set; }
 
+        /// <summary>
+        /// Location of the user
+        /// </summary>
         public Tuple<double, double> Location { get; private set; }
 
+        /// <summary>
+        /// The boudning box of a map view
+        /// </summary>
         public Tuple<double, double, double, double> MapView { get; private set; }
     }
 }
