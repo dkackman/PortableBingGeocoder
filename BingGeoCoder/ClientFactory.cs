826500163e64c28bb7875c96050b807f1422eb86
Don Kackman <dkackman@gmail.com>
2015-07-30 19:29:01 -0500

    add timeout test and test files for other unit tests

---
+++ BingGeoCoder/ClientFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+
+using Microsoft.Practices.ServiceLocation;
+
+namespace BingGeocoder
+{
+    static class ClientFactory
+    {
+        public static HttpClient CreateClient(string user_agent)
+        {
+            bool disposeHandler;
+            var handler = GetHandler(out disposeHandler);
+
+            var client = new HttpClient(handler, disposeHandler);
+
+            if (handler is HttpClientHandler && ((HttpClientHandler)handler).SupportsTransferEncodingChunked())
+            {
+                client.DefaultRequestHeaders.TransferEncodingChunked = true;
+            }
+
+            client.BaseAddress = new Uri("http://dev.virtualearth.net/REST/v1/", UriKind.Absolute);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            ProductInfoHeaderValue productHeader = null;
+            if (!string.IsNullOrEmpty(user_agent) && ProductInfoHeaderValue.TryParse(user_agent, out productHeader))
+            {
+                client.DefaultRequestHeaders.UserAgent.Clear();
+                client.DefaultRequestHeaders.UserAgent.Add(productHeader);
+            }
+
+            return client;
+        }
+
+        private static HttpMessageHandler GetHandler(out bool dispose)
+        {
+            try
+            {
+                if (ServiceLocator.IsLocationProviderSet)
+                {
+                    var handler = ServiceLocator.Current.GetInstance<HttpMessageHandler>();
+                    if (handler != null)
+                    {
+                        dispose = false;
+                        return handler;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            var clientHandler = new HttpClientHandler();
+            if (clientHandler.SupportsAutomaticDecompression)
+            {
+                clientHandler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            }
+
+            dispose = true;
+            return clientHandler;
+        }
+    }
+}
---
+++ GeoCoderTests/RetryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BingGeocoder;
+
+using FakeHttp;
+
+namespace GeoCoderTests
+{
+    [TestClass]
+    [DeploymentItem(@"FakeResponses\")]
+    public class RetryTests
+    {
+        private class Callbacks : ResponseCallbacks
+        {
+
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [ExpectedException(typeof(TimeoutException))]
+        public async Task ResponseTimesoutOnBusyHeader()
+        {
+            var mockFolder = TestContext.DeploymentDirectory; // the folder where the unit tests are running
+            var captureFolder = Path.Combine(TestContext.TestRunDirectory, @"..\..\FakeResponses\"); // kinda hacky but this should be the solution folder
+
+            // here we don't want to serialize or include our api key in response lookups so
+            // pass a lambda that will indicate to the serialzier to filter that param out
+            var store = new FileSystemResponseStore(mockFolder, captureFolder);
+            using (var handler = new FakeHttpMessageHandler(store))
+            {
+                var client = new GeoCoder(APIKEY.Key, 4, 1000, "Portable-Bing-GeoCoder-UnitTests/1.0", handler: handler);
+                var coord = await client.GetCoordinate(null, null, null, "55106", "US");
+
+                Assert.IsNotNull(coord);
+            }
+        }
+    }
+}
